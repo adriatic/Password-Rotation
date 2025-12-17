a3dddeddde87d95bbe602df71e4dc65428f22fe5
Nikolaj Ivancic <nik@congral.us>
2025-12-16 20:40:16 -0800

    2.2 done

--- backend/db.ts
+++ backend/db.ts
@@ -1,3 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
